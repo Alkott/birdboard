2f389cfca63d521be6f31dd1b01b119031f92256
Vitaliy Kulyavets <vkulyavets@cleack.com>
2019-07-07 19:40:12 +0300

    basic project logic added

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/projects', 'ProjectsController@index');
+Route::get('/projects/{project}', 'ProjectsController@show');
+Route::post('/projects', 'ProjectsController@store');
